6c6416d962851454753a284f88338b47fa8d1d7e
tm-315 <topguru315@gmail.com>
2017-09-23 19:05:07 +0300

    'Flatty' bootstrap theme integration iteration #1;

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './css/main.css';
 
+//load fonts
 import WebFont from 'webfontloader';
 
 WebFont.load({
@@ -13,6 +14,9 @@ WebFont.load({
     families: ['Lato:300,400,900']
   }
 });
+
+//load images
+import Image_Ipad_Hand from '../assets/img/ipad-hand.png'
 
 //Finish me!
 
@@ -38,9 +42,30 @@ export default class App extends Component {
   </div>
   </div>;
 
+  const HeadWrap = <div id="headerwrap">
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-6">
+        <h1>Make your landing page<br/>
+        look really good.</h1>
+        <form className="form-inline" role="form">
+          <div className="form-group">
+            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter your email address"/>
+          </div>
+          <button type="submit" className="btn btn-warning btn-lg">Invite Me!</button>
+        </form>					
+      </div>
+      <div className="col-lg-6">
+        <img className="img-responsive" src={Image_Ipad_Hand} alt="ipad-hand.png"/>
+      </div>
+    </div>
+  </div>
+</div>;
+
     return (
       <div>
       {NavBar}
+      {HeadWrap}
       <SearchHeader/>
       <Jobs/>
       </div>
