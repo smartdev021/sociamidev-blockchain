fdcef55de03be520b6c50d448ba34fa401c0f5fb
tm-315 <topguru315@gmail.com>
2017-09-23 18:08:25 +0300

    'Flatty' bootstrap theme integration iteration #1;

--- src/App.js
+++ src/App.js
@@ -6,13 +6,41 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './css/main.css';
 
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Lato:300,400,900']
+  }
+});
+
 //Finish me!
 
 export default class App extends Component {
   render() {
+
+    /*Nav Bar*/
+    const NavBar = <div className="navbar navbar-default navbar-fixed-top">
+    <div className="container">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="#"><b>FLATTY</b></a>
+      </div>
+      <div className="navbar-collapse collapse">
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="#">Already a member?</a></li>
+        </ul>
+    </div>
+  </div>
+  </div>;
+
     return (
       <div>
-      <h1>Indeed Jobs List.</h1>
+      {NavBar}
       <SearchHeader/>
       <Jobs/>
       </div>
