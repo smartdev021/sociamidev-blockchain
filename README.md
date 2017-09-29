dcddb224f7ddb3718e97df7f25866baf1d2862eb
tm-315 <topguru315@gmail.com>
2017-09-30 00:45:49 +0800

    Create python script to deploy to AWS from local dev machine

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+## How to deploy from dev local machine
+### Pre-requisite:
+- Python is installed. If not, download and install from the following link https://www.python.org/downloads/
+- boto3 is installed. If not, run the follow command: ```pip install boto3==1.3.0```
+### Configure AWS Credential
+Open/create file ~/.aws/config , add these lines:
+```
+[default]
+aws_access_key_id = AWS_ACCESS_KEY_ID
+aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+region=AWS_REGION
+```
+### Run command to deploy to AWS
+```
+python deploy.py Staging dist/artifact.zip
+```
+
+
+
+
 >## A Big Update Is Coming
 
 >React Hot Loader 3 is [on the horizon](https://github.com/gaearon/react-hot-loader/pull/240), and you can try it today ([boilerplate branch](https://github.com/gaearon/react-hot-boilerplate/pull/61), [upgrade example](https://github.com/gaearon/redux-devtools/commit/64f58b7010a1b2a71ad16716eb37ac1031f93915)). It fixes some [long-standing issues](https://twitter.com/dan_abramov/status/722040946075045888) with both React Hot Loader and React Transform, and is intended as a replacement for both. The docs are not there yet, but they will be added before the final release. For now, [this commit](https://github.com/gaearon/redux-devtools/commit/64f58b7010a1b2a71ad16716eb37ac1031f93915) is a good reference.
---
+++ deploy.py
@@ -0,0 +1,132 @@
+# Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under the License.
+"""
+A BitBucket Builds template for deploying an application revision to AWS CodeDeploy
+narshiva@amazon.com
+v1.0.0
+"""
+from __future__ import print_function
+import os
+import sys
+from time import strftime, sleep
+import boto3
+from botocore.exceptions import ClientError
+
+APPLICATION_NAME        = 'Sociami'
+DEPLOYMENT_CONFIG       = 'CodeDeployDefault.OneAtATime'
+S3_BUCKET               = 'sociamibucket'
+VERSION_LABEL = strftime("%Y%m%d%H%M%S")
+BUCKET_KEY = APPLICATION_NAME + '/' + VERSION_LABEL + \
+    '-bitbucket_builds.zip'
+
+def upload_to_s3(artifact):
+    """
+    Uploads an artifact to Amazon S3
+    """
+    try:
+        client = boto3.client('s3')
+    except ClientError as err:
+        print("Failed to create boto3 client.\n" + str(err))
+        return False
+    try:
+        client.put_object(
+            Body=open(artifact, 'rb'),
+            Bucket=S3_BUCKET,
+            Key=BUCKET_KEY
+        )
+    except ClientError as err:
+        print("Failed to upload artifact to S3.\n" + str(err))
+        return False
+    except IOError as err:
+        print("Failed to access artifact.zip in this directory.\n" + str(err))
+        return False
+    return True
+
+def deploy_new_revision(env):
+    """
+    Deploy a new application revision to AWS CodeDeploy Deployment Group
+    """
+    try:
+        client = boto3.client('codedeploy')
+    except ClientError as err:
+        print("Failed to create boto3 client.\n" + str(err))
+        return False
+
+    try:
+        response = client.create_deployment(
+            applicationName=APPLICATION_NAME,
+            deploymentGroupName=env,
+            revision={
+                'revisionType': 'S3',
+                's3Location': {
+                    'bucket': S3_BUCKET,
+                    'key': BUCKET_KEY,
+                    'bundleType': 'zip'
+                }
+            },
+            deploymentConfigName=DEPLOYMENT_CONFIG,
+            description='New deployment from local dev machine',
+            ignoreApplicationStopFailures=True
+        )
+    except ClientError as err:
+        print("Failed to deploy application revision.\n" + str(err))
+        return False     
+           
+    """
+    Wait for deployment to complete
+    """
+    while 1:
+        try:
+            deploymentResponse = client.get_deployment(
+                deploymentId=str(response['deploymentId'])
+            )
+            deploymentStatus=deploymentResponse['deploymentInfo']['status']
+            if deploymentStatus == 'Succeeded':
+                print ("Deployment Succeeded")
+                return True
+            elif (deploymentStatus == 'Failed') or (deploymentStatus == 'Stopped') :
+                print ("Deployment Failed. Please check AWS CodeDeploy for more detailed.")
+                return False
+            elif (deploymentStatus == 'InProgress') or (deploymentStatus == 'Queued') or (deploymentStatus == 'Created'):
+                continue
+        except ClientError as err:
+            print("Failed to deploy application revision.\n" + str(err))
+            return False      
+    return True
+
+def validate_command():
+    """
+    get the deployment environment name input from command line.
+    """
+    if (len(sys.argv) <3):
+        print("Please specify environemnt path to the artifact to deploy.\n" +\
+        "Example:\n" +\
+        "\tpython deploy.py Staging dist/artifact.zip")
+        sys.exit(1)
+    if not sys.argv[1] in ['Staging', 'Production']:
+        print("The environment is not correct, please specify the correct environment to deploy. \n[Staging, Production]")
+        sys.exit(1)
+    if not os.path.isfile(sys.argv[2]):
+        print("Artifact location is not correct, please check again.")
+        sys.exit(1)
+
+def main():
+    validate_command()
+    print("Start uploading...");
+    if not upload_to_s3(sys.argv[2]):
+        sys.exit(1)
+    print("Upload sucessfully.\n");
+    print("Start deploying...");
+    if not deploy_new_revision(sys.argv[1]):
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
