a6fc9f2a73c0532a34df885814e9c363c1069f25
tm-315 <topguru315@gmail.com>
2017-10-02 18:26:56 +0800

    Update wordings on main container

--- src/components/ThemeMainContainer.js
+++ src/components/ThemeMainContainer.js
@@ -10,29 +10,29 @@ class ThemeMainContainer extends React.Component {
       <div className="container">
 <div className="row mt centered">
   <div className="col-lg-6 col-lg-offset-3">
-    <h1>Your Landing Page<br/>Looks Wonderful Now.</h1>
-    <h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h3>
+    <h1>The Social Learning Economy</h1>
+    <h3>Make every step in your journey count.</h3>
   </div>
 </div>
 
 <div className="row mt centered">
   <div className="col-lg-4">
     <img src="https://sociamibucket.s3.amazonaws.com/assets/images/ser01.png" width="180" alt="ser01.png"/>
-    <h4>1 - Browser Compatibility</h4>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+    <h4>Get more awareness</h4>
+    <p>Find out more about whats happening around you</p>
   </div>
 
   <div className="col-lg-4">
     <img src="https://sociamibucket.s3.amazonaws.com/assets/images/ser02.png" width="180" alt="ser02.png"/>
-    <h4>2 - Email Campaigns</h4>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+    <h4>Meaningful Networks</h4>
+    <p>Connect with others to share information and learn together.</p>
 
   </div>
 
   <div className="col-lg-4">
     <img src="https://sociamibucket.s3.amazonaws.com/assets/images/ser03.png" width="180" alt="ser03.png"/>
-    <h4>3 - Gather Your Notes</h4>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
+    <h4>Incentives to take action</h4>
+    <p>Get motivated with rewards and track your journey.</p>
 
   </div>
 </div>
