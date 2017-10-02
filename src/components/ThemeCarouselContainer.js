596211a082e47e7d7fce6120a193ab36d9a25c8f
tm-315 <topguru315@gmail.com>
2017-10-02 21:44:02 +0800

    updating wordings for carousel container

--- src/components/ThemeCarouselContainer.js
+++ src/components/ThemeCarouselContainer.js
@@ -9,8 +9,8 @@ class ThemeCarouselContainer extends React.Component {
     return (<div className="container">
 		<div className="row mt centered">
 			<div className="col-lg-6 col-lg-offset-3">
-				<h1>Flatty is for Everyone.</h1>
-				<h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h3>
+				<h1>Our Technology.</h1>
+				<h3>Everything in one place. Simplify your search.</h3>
 			</div>
 		</div>
 		<div className="row mt centered">
