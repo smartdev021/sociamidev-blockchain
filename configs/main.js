b5ef5248b34696d49134f4843f2732a5fb101976
tm-315 <topguru315@gmail.com>
2017-10-15 01:05:07 +0300

    small fixes

--- configs/main.js
+++ configs/main.js
@@ -3,7 +3,7 @@ const BACKEND_URL_REMOTE_PROD = 'http://13.59.19.153:3000';
 const BACKEND_URL_REMOTE_STAGING = 'http://13.59.19.153:8080';
 
 var ConfigMain = {
-    BackendURL: BACKEND_URL_REMOTE_STAGING,
+    BackendURL: BACKEND_URL_REMOTE_PROD,
     S3BucketURL: 'https://sociamibucket.s3.amazonaws.com'
 }
 
