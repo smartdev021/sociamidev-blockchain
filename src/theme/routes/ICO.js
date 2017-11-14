9679d7e52baaec93c3153631a88d254e2ecc2559
tm-315 <topguru315@gmail.com>
2017-11-14 07:30:12 +0200

    Loading the PDF: for now, just added a luink to PDF

--- src/theme/routes/ICO.js
+++ src/theme/routes/ICO.js
@@ -3,10 +3,14 @@
 */
 import React from 'react';
 
-import Sample from '~/src/theme/components/PdfViewer'
+//import PdfViewer from '~/src/theme/components/PdfViewer'
 
 function ICO(props) {
-  return <Sample/>;
+  //return <PdfViewer/>;
+  return (
+  <div>
+    <a href="http://sociamibucket.s3.amazonaws.com/pdf/Latest%201110%20FTCHK%20Pitch.pdf" target="_blank"><h2>Click to read more...</h2></a>
+  </div>);
 }
 
 export default ICO;
