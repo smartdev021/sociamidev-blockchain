f11ed9c73015d3bbb5663d3675b212af5fdd0c5f
tm-315 <topguru315@gmail.com>
2017-11-15 09:02:29 +0200

    Project Manager: next iteration - started creating 'New Project' popup

--- src/theme/components/PopupNewProject.js
+++ src/theme/components/PopupNewProject.js
@@ -26,7 +26,8 @@ class PopupNewProject extends React.Component {
       this.setState(copy);
 
       Modal.defaultStyles.content.border = "7px solid grey";
-      Modal.defaultStyles.content.background = "transparent";
+      Modal.defaultStyles.content.background = "white";
+      Modal.defaultStyles.content.color = "initial";
       Modal.defaultStyles.content.overflow = "visible";
       Modal.defaultStyles.content.padding = '0';
       Modal.defaultStyles.content["minWidth"] = '260px';
@@ -36,7 +37,7 @@ class PopupNewProject extends React.Component {
       Modal.defaultStyles.content["marginRight"] = 'auto';
       Modal.defaultStyles.content["left"] = '0';
       Modal.defaultStyles.content["right"] = '0';
-      Modal.defaultStyles.content["width"] = '800px';
+      Modal.defaultStyles.content["width"] = '600px';
     }
 
     componentWillUnMount() {
@@ -46,11 +47,41 @@ class PopupNewProject extends React.Component {
     renderModal() {
       return (
         <Modal  isOpen={this.props.modalIsOpen} onRequestClose={() => this.props.onCloseModal()} contentLabel={"Create New Task"}>
-          <div className="container-fluid default-popup-details">
+          <div className="container-fluid popup-new-project">
             <a href='#' className="glyphicon glyphicon-remove" onClick={() => this.props.onCloseModal()}></a>
             <div className="row">
               <div className="col-lg-12">
-                <h2>Create a New</h2>
+                <h5>Add a new Project</h5>
+                <div>Do you have a project you are working on that 
+                  you would like to share or get help with your friends</div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="create-project-desc-column">
+                  <div className="glyphicon glyphicon-bitcoin"></div>
+                  <div><b>Use your Tokens</b></div>
+                  <p>You can use tokens you have earned to create projects that your heart desires.</p>
+                  </div>
+                </div>
+              <div className="col-lg-6">
+                <div className="create-project-desc-column">
+                  <div className="glyphicon glyphicon glyphicon-user"></div>
+                  <div><b>Your Friends</b></div>
+                  <p>These projects contain Milestones that create tasks for your friends to help.</p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-12">
+                <span>Tip: you can even create your desired final year project here!</span>
+                <hr></hr>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-12">
+                <h5><span className="badge">1</span>Project Details</h5>
+                <div>Tell us more about your project</div>
               </div>
             </div>
           </div>
