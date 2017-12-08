f9f6ab6d6af003b8f2491365d12ae11034f1515f
tm-315 <topguru315@gmail.com>
2017-12-08 08:42:52 +0700

    New UI: 'Main' now split to separate modules

--- src/Main_New_Theme.js
+++ src/Main_New_Theme.js
@@ -5,15 +5,19 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom'
 //THEME NEW
-import HomePageThemeNew from '~/src/theme_new/HomePage.js';
 import SignUpFormPopup from  '~/src/authentication/SignUpForm';
+
+import SidebarLeft from '~/src/theme_new/SidebarLeft.js';
+import NavTop from '~/src/theme_new/NavTop.js';
+import TrendScanner from '~/src/theme_new/TrendScanner.js';
+import ThemeHeader from '~/src/theme_new/ThemeHeader.js';
 
 //<Route exact path='/howItWorks' render={routeProps => <HowItWorks {...routeProps}{...this.props}/>} />
 
 class Main extends React.Component {
   render() {
     return (
-      <div>
+      <div id="wrapper">
       {this.props.isSignUpFormOpen ? <SignUpFormPopup 
           modalIsOpen={this.props.isSignUpFormOpen}
           isAuthorized={this.props.isAuthorized}
@@ -22,15 +26,49 @@ class Main extends React.Component {
           onHandleSignUpLinkedIn={()=>this.props.onHandleSignUpLinkedIn()}
           pathname={this.props.pathname}
       />: null}
-      <Switch>
-        <Route path='/' render={routeProps => <HomePageThemeNew {...routeProps}{...this.props}/>} />)}/>
-  )}/>
-
-      </Switch>
-    </div>
+        <ThemeHeader/>
+        <div className="container-fluid">
+          <div className="row">
+            <div id="wrapper-content">
+              <div className="col-lg-2">
+                <SidebarLeft/>
+              </div>
+              <div className="col-lg-10">
+                <main className>
+                  <div className="container-fluid content-top">
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <NavTop/>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <div id="main-content_1-status">
+                          project manager / stock trading / investment banking / risk analysis
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="container-fluid">
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <div id="main-content_1">
+                          <Switch>
+                            <Route exact path='/' render={routeProps => <TrendScanner {...routeProps}{...this.props}/>} />
+                            <Route path='/themeNew' render={routeProps => <TrendScanner {...routeProps}{...this.props}/>} />
+                          </Switch>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </main>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
-
 }
 
 export default Main;
