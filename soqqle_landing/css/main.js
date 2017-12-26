a9b7d5be2753729e23b6e5d0649835620522d442
tm-315 <topguru315@gmail.com>
2017-12-26 13:02:52 +0700

    Soqqle landing: Added name and email fields

--- soqqle_landing/css/main.js
+++ soqqle_landing/css/main.js
@@ -9,7 +9,7 @@ $(document) ['ready'](function () {
   $('.social') ['css']('width', SocialSize);
   ReAlign();
   $('.videoDivider') ['hide']();
-  $('.joinusButton') ['click'](openSurvey);
+  $('.joinusButton') ['click'](subscribe);
 });
 
 function openSurvey() {
@@ -17,12 +17,25 @@ function openSurvey() {
 }
 
 function subscribe() {
-    if (validateEmail($('.mail') ['val']()) === false) {
-      $('.formvalidate') ['fadeIn']();
+  var isNameValid = validateName($('.name') ['val']());
+  var isEmailValid = validateEmail($('.mail') ['val']());
+
+  if (!isNameValid) {
+    $('.formvalidate#form-validate-name') ['fadeIn']();
+  }
+
+  if (!isEmailValid) {
+    $('.formvalidate#form-validate-mail') ['fadeIn']();
+  }
+
+    if (!isNameValid || !isEmailValid) {
+      return
     } else {
       $['ajax']({
         url: 'mail.php?email=' + $('.mail') ['val'](),
         success: function (data) {
+          console.log("data");
+          console.dir(data);
           if (data == 'OK') {
             $('.formsent') ['fadeIn']();
           };
@@ -40,4 +53,8 @@ function ReAlign() {
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re['test'](email);
+};
+
+function validateName(name) {
+  return name != "";
 };
