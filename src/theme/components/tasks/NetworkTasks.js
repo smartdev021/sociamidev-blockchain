2161c402140591a960d985189e4e2a2cb0cad463
tm-315 <topguru315@gmail.com>
2018-01-02 13:20:42 +0700

    Workflow Management: Format output of Hangout

--- src/theme/components/tasks/NetworkTasks.js
+++ src/theme/components/tasks/NetworkTasks.js
@@ -5,6 +5,67 @@ import React from 'react';
 
 import ActionLink from '~/src/components/common/ActionLink'
 
+import TaskTypes from "~/src/common/TaskTypes"
+
+const DayFromNumber = (dayNum)=> {
+  const DayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  return DayNames[dayNum];
+}
+
+const MonthFromNumber = (monthNum)=> {
+  const MonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Dec'];
+
+  return MonthNames[monthNum];
+}
+
+const Hours12 = (date) => { return (date.getHours() + 24) % 12 || 12; }
+
+const RenderSingleTask = (task, i, props)=> {
+  if (task.type == TaskTypes.HANGOUT) {
+    const date = new Date(task.metaData.time);
+
+    const dateNow = new Date(Date.now());
+    const dateTomorrow = new Date(Date.now() + (60 * 60 * 24));
+
+    const Noon = new Date(date.getFullYear(), date.getMonth(),date.getDate(), 12, 0, 0);
+    const AmPm = (date.getTime() < Noon.getTime()) ? 'am' : 'pm';
+
+    const Hours = String(Hours12(date)) + AmPm;
+    let time = "";
+
+    if (dateNow.getDay() == date.getDay() && dateNow.getMonth() == date.getMonth() && dateNow.getFullYear() == date.getFullYear()) {
+      time = `${Hours} today`;
+    }
+    else if (dateTomorrow.getDay() == date.getDay() && dateTomorrow.getMonth() == date.getMonth() && dateTomorrow.getFullYear() == date.getFullYear()) {
+      time = `${Hours} tomorrow`;
+    }
+    else {
+      time = `${Hours} on ${DayFromNumber(date.getUTCDay())} (${date.getDay() + 1} ${MonthFromNumber(date.getMonth())})`; 
+    }
+
+    
+    return (
+      <li key={i}>
+        <div>
+          <ActionLink href="#" onClick={()=>props.handleOpenConfirmTaskDetailsPopup(task)}>
+            {task.creator.firstName} is looking to Hangout for <br/>roadmap: {task.metaData.subject.roadmap.name} at {time}
+          </ActionLink>
+        </div>
+      </li>
+    );
+  }
+  else {
+    return (
+      <li key={i}>
+        <div>
+          <ActionLink href="#" onClick={()=>props.handleOpenConfirmTaskDetailsPopup(task)}>{task.name}</ActionLink>
+        </div>
+      </li>
+    );
+  }
+};
+
 const NetworkTasks = (props) => {
     
   let foundTasks = [];
@@ -29,12 +90,7 @@ const NetworkTasks = (props) => {
         <ul id="tasks-scanner-list-tasks">
         {
           foundTasks.map(function(task, i) {
-            return (
-              <li key={i}>
-                <div>
-                  <ActionLink href="#" onClick={()=>props.handleOpenConfirmTaskDetailsPopup(task)}>{task.name}</ActionLink>
-                </div>
-              </li>);
+            return RenderSingleTask(task, i, props);
             })
           }
         </ul>
