95374d37ea37de83a734734f6378b3ef4a71ae7d
tm-315 <topguru315@gmail.com>
2018-01-07 15:56:36 +0700

    Workflow Management: Hangout questions form added

--- src/theme/components/tasks/TaskBrowser.js
+++ src/theme/components/tasks/TaskBrowser.js
@@ -20,9 +20,100 @@ class TaskBrowser extends React.Component {
     super(props);
   }
 
+  renderQuestions() {
+    const DummyQuestions = [
+      {
+        title: "What is the future of Fintech?", participants: [
+          {_id: "5s323treg4d323t", firstName: "You"},
+          {_id: "5s37h23treg4d33g23t", firstName: "John"},
+        ] 
+      },
+      {
+        title: "What are some examples of usage of Fintech?", participants: [
+          {_id: "5s323treg4d323t", firstName: "You"},
+          {_id: "5s37h23treg4d33g23t", firstName: "John"},
+        ] 
+      },
+      {
+        title: "What are the key challenges of Fintech?", participants: [
+          {_id: "5s323treg4d323t", firstName: "You"},
+          {_id: "5s37h23treg4d33g23t", firstName: "John"},
+        ] 
+      },
+      {
+        title: "What are the key challenges of Fintech?", participants: [
+          {_id: "5s323treg4d323t", firstName: "You"},
+          {_id: "5s37h23treg4d33g23t", firstName: "John"},
+        ] 
+      },
+      {
+        title: "What are the key challenges of Fintech?", participants: [
+          {_id: "5s323treg4d323t", firstName: "You"},
+          {_id: "5s37h23treg4d33g23t", firstName: "John"},
+        ] 
+      },
+      {
+        title: "What are the key challenges of Fintech?", participants: [
+          {_id: "5s323treg4d323t", firstName: "You"},
+          {_id: "5s37h23treg4d33g23t", firstName: "John"},
+        ] 
+      },
+      {
+        title: "What are the key challenges of Fintech?", participants: [
+          {_id: "5s323treg4d323t", firstName: "You"},
+          {_id: "5s37h23treg4d33g23t", firstName: "John"},
+        ] 
+      },
+    ];
+
+    return (
+    <div id="questions-list">
+      <div className="container-fluid">
+        {
+          DummyQuestions.map(function(question, i) {
+            return (
+              <div className="row" key={i}>
+                <div className="col-lg-12">
+                  {i + 1}) {question.title}
+                </div>
+                <div className="col-lg-6">
+                  <div>{question.participants[0].firstName}</div>
+                  <div className="form-group">
+                    <textarea id="answer_your" className="form-control validate-field required question-text-area" 
+                      name="answer_your" onChange={(e)=>{}} />
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div>{question.participants[1].firstName}</div>
+                  <div className="form-group">
+                    <textarea id="answer_partner" className="form-control validate-field required question-text-area" 
+                      name="answer_partner" onChange={(e)=>{}} />
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
+  </div>
+    )
+  }
+
   render() {
     return (
       <div id="main-content_1">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12">
+              <h1>Your meeting with John</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12">
+              {this.renderQuestions()}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
