cd44d0d7c96070e4a941cb2fff009174995baf5d
tm-315 <topguru315@gmail.com>
2018-01-14 18:39:35 +0200

    new to old user profile

--- src/Main.js
+++ src/Main.js
@@ -30,8 +30,6 @@ import ConnectionsView from '~/src/theme/ConnectionsView.js';
 import "~/src/theme/css/main.css";
 import UserProfile from '~/src/theme/UserProfile.js';
 
-import NewUserProfile from '~/src/theme/NewUserProfile.js';
-
 import Privacy from '~/src/theme/Privacy.js';
 
 
