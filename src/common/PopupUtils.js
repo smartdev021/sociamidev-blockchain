f802b2eb658203290af1aedc9375f89ab6d5933f
tm-315 <topguru315@gmail.com>
2018-02-17 12:46:13 +0700

    PopupUtils. Added error message

--- src/common/PopupUtils.js
+++ src/common/PopupUtils.js
@@ -3,7 +3,10 @@ const ParentElementID = 'popup-root';
 export const getPopupParentElement = () => {
     let parentElement = document.getElementById(ParentElementID);
 
-
+    if (!parentElement) {
+        console.log(`%cParent with id: ${ParentElementID}`, "background:red;color:white;");
+        parentElement = document.body;
+    }
 
     return parentElement;
 }
