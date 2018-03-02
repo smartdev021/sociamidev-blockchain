fc01b10f20de20aff983618f9910056b8ea5354c
tm-315 <topguru315@gmail.com>
2018-03-02 12:45:30 +0700

    Chracter Selection Popup

--- src/character-creation/CharacterSelection.js
+++ src/character-creation/CharacterSelection.js
@@ -14,32 +14,38 @@ import "./characterSelection.css"
 const charactersData = [
   {
     name: "Ashe", 
-    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Ashe.png", 
+    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Ashe.png",
+    imageBig: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Ashe_big.png",
     descriptionText: "Ashe is a kin enthusias is in robotics and AI"
   },
   {
     name: "Kaye", 
-    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Kaye.png", 
+    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Kaye.png",
+    imageBig: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Kaye_big.png",
     descriptionText: "Kaye is a kin enthusias is in robotics and AI"
   },
   {
     name: "Leo", 
-    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Ashe.png", 
+    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Leo.png",
+    imageBig: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Leo_big.png",
     descriptionText: "Leo is a kin enthusias is in robotics and AI"
   },
   {
     name: "Leona", 
-    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Leona.png", 
+    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Leona.png",
+    imageBig: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Leona_big.png",
     descriptionText: "Leona is a kin enthusias is in robotics and AI"
   },
   {
     name: "Max", 
-    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Max.png", 
+    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Max.png",
+    imageBig: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Max_big.png",
     descriptionText: "Max is a kin enthusias is in robotics and AI"
   },
   {
     name: "Nelson", 
-    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Nelson.png", 
+    image: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Nelson.png",
+    imageBig: "http://sociamibucket.s3.amazonaws.com/assets/character_creation/character_icons/Nelson_big.png",
     descriptionText: "Nelson is a kin enthusias is in robotics and AI"
   },
 ];
@@ -64,7 +70,7 @@ class CharacterSelection extends React.Component {
       Modal.defaultStyles.content["minWidth"] = 'initial';
       Modal.defaultStyles.content["maxWidth"] = 'initial';
       Modal.defaultStyles.content["overflowX"] = "hidden";
-      Modal.defaultStyles.content["overflowY"] = "fixed";
+      Modal.defaultStyles.content["overflowY"] = "hidden";
       Modal.defaultStyles.content["marginLeft"] = 'auto';
       Modal.defaultStyles.content["marginRight"] = 'auto';
       Modal.defaultStyles.content["left"] = '0';
@@ -89,16 +95,19 @@ class CharacterSelection extends React.Component {
 
     renderCharacters(characters, firstIndex, lastIndex) {
       return (
-        <div>
+        <div className="row">
           {
             characters.map((character, i) => {
               if (i >= firstIndex && i <= lastIndex) {
                 return (
                   <div className="col-lg-12 col-md-4 col-sm-4 col-xsm-12" key={i}>
-                    <ActionLink onClick={()=>this.handleChangeSelectedChatacter(i)} className={`character-selection-button ${this.state.selectedIndex == i ? 
-                      'character-selected' : ""}`}>
-                      <img src={charactersData[i].image}/>
-                    </ActionLink>
+                    <div className="character-selection-button-container">
+                      <ActionLink onClick={()=>this.handleChangeSelectedChatacter(i)} 
+                        className={`character-selection-button ${this.state.selectedIndex == i ? 
+                        'character-selected' : ""}`}>
+                        <img src={charactersData[i].image}/>
+                      </ActionLink>
+                    </div>
                   </div>
                 )
               }
@@ -117,6 +126,9 @@ class CharacterSelection extends React.Component {
             <div id="character-selection-container">
               <div id="character-selection-container-inner">
                 <div className="container-fluid">
+                  <div id="character-selection-image-big">
+                    <img src={charactersData[this.state.selectedIndex].imageBig}/>
+                  </div>
                   <div className="row">
                     <div className="col-lg-12">
                       <div className="text-center" id="character-seelection-header">
@@ -125,13 +137,13 @@ class CharacterSelection extends React.Component {
                     </div>
                   </div>
                   <div className="row">
-                    <div className="col-lg-2 col-mg-12">
+                    <div className="col-lg-2 col-md-12">
                       {this.renderCharacters(charactersData, 0, 2)}
                     </div>
-                    <div className="col-lg-8 col-mg-12">
+                    <div className="col-lg-8 col-md-12">
                       <div className="text-center" id="character-info">
                         <div id="character-name">
-                          <h4 className="text-uppercase">Ashe</h4>
+                          <h4 className="text-uppercase">{charactersData[this.state.selectedIndex].name}</h4>
                         </div>
                         <div id="character-description">
                           <p>{charactersData[this.state.selectedIndex].descriptionText}</p>
@@ -140,7 +152,7 @@ class CharacterSelection extends React.Component {
                         </div>
                       </div>
                     </div>
-                    <div className="col-lg-2 col-mg-12">
+                    <div className="col-lg-2 col-md-12">
                       {this.renderCharacters(charactersData, 3, charactersData.length - 1)}
                     </div>
                   </div>
