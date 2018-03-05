aadc6d13cb3f4f79b1cdeff4d7a300e54bc68bd2
tm-315 <topguru315@gmail.com>
2018-03-05 14:41:48 +0530

    The user with the latest message will be moved on to top of Chat list and made the chat window open when the user receives the Welcome message from chatbot

--- src/components/chat/User.js
+++ src/components/chat/User.js
@@ -6,34 +6,19 @@ class User extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {selectedItem: '-2', unreadCount: 0, userSelected:""};
-  }
-
-  componentWillReceiveProps(nextProps){
-    if(this.props.lastMessageRec !== nextProps.lastMessageRec){
-      var tempLastMessageRec = nextProps.lastMessageRec;
-      if(nextProps.userID == tempLastMessageRec.sender){
-        var tempUnreadCount = this.state.unreadCount;
-        tempUnreadCount = tempUnreadCount + 1;
-        this.state.unreadCount = tempUnreadCount;
-      }
-    }
-    if(this.state.userSelected == nextProps.selectedUser && this.state.userSelected !== ""){
-      this.state.unreadCount = 0;
-    }
+    this.state = {userSelected:""};
   }
 
   onItemClick(event) {
     event.preventDefault(); 
     this.state.userSelected = event.currentTarget.id;
-    this.state.selectedItem = event.currentTarget.dataset.id;
-    this.props.onTab(event.currentTarget.id,event.currentTarget.dataset.user,event.currentTarget.dataset.id);
+    this.props.onTab(event.currentTarget.id,event.currentTarget.dataset.user);
   }
 
   render() {
-    const tempClasses = this.state.unreadCount == 0 ? "messageCountContainerHide" : "messageCountContainerShow";
+    const tempClasses = !(this.props.unreadCount[this.props.userID]) ? "messageCountContainerHide" : this.props.unreadCount[this.props.userID] == 0 ? "messageCountContainerHide" : "messageCountContainerShow";
     const messageCountContainerClasses = `messageCountContainer ${tempClasses}`;
-    const tabClass = String(this.state.selectedItem) === String(this.props.selectedTab) ? "personSelected" : "person";
+    const tabClass = String(this.props.userID) === String(this.props.selectedUser) ? "personSelected" : "person";
     const classes = `${tabClass}` ;
     var fullname = this.props.firstName + " " + this.props.lastName;
     var imgSrc = "";
@@ -53,13 +38,18 @@ class User extends React.Component {
     else if(this.props.userType == "chatbot"){
       imgSrc = "http://blog.newrelic.com/wp-content/uploads/chatbot-300x300.jpg";
     }
+
+    var unreadCountTag = "";
+    if(this.props.unreadCount[this.props.userID]){
+      unreadCountTag = String(this.props.unreadCount[this.props.userID]);
+    }
     return (
           <div data-id={`${this.props.tabKey}`} className={classes} onClick={(event)=>this.onItemClick(event)}  id={`${this.props.userID}`} data-user={`${fullname}`}>
             <img src={imgSrc} alt="" className="profilePic"/>
             <span className="name">{ fullname } <img src={statusImgSrc} className="statusDot"/></span>
             <span className="time"><TimeAgo date={this.props.lastMessageTimeStamp} minPeriod={60}/></span>
             <span className="preview">{ReactHtmlParser(showLastMessage)}</span>
-            <div className={messageCountContainerClasses}><span className="messageCount">{this.state.unreadCount}</span></div>
+            <div className={messageCountContainerClasses}><span className="messageCount">{unreadCountTag}</span></div>
           </div>
     );
   }
