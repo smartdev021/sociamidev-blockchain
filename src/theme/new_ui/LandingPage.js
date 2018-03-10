a42134d1999bd776d88552778a874034d8ab87cc
tm-315 <topguru315@gmail.com>
2018-03-10 16:20:33 +0530

    Changes to home page

--- src/theme/new_ui/LandingPage.js
+++ src/theme/new_ui/LandingPage.js
@@ -74,14 +74,7 @@ class LandingPage extends React.Component {
               </div>
               <div className="col-xs-6 pull-right">
                 <div className="pull-right" id="landing-header-links">
-                  <ActionLink href="#" onClick={()=> this.props.openSignUpForm()} 
-                    className="btn-base-landing btn-yellow-landing btn-login-landing">
-                    Sign in
-                  </ActionLink>
                   <div className="text-right">
-                    <ActionLink id="link-create-acc" href="#" onClick={()=> this.startCharacterCreation()} className="">
-                      Create an Account Instead
-                    </ActionLink>
                   </div>
                 </div>
               </div>
@@ -96,11 +89,13 @@ class LandingPage extends React.Component {
                       prepare and solve challenges to unlock the next-generation Human Renaissance.
                   </p>
                   <p>
-                    <a href="#" className="btn-base-landing btn-red-landing only-line" data-toggle="modal" data-target="#token">
-                      <span className="font-small">Register for</span><br/>Token Events Updates</a>
-                      <a href="#" className="btn-base-landing btn-red-landing" data-toggle="modal" data-target="#alpha">
-                        <span className="font-small">Register for</span> alpha</a>
-                      </p>
+                        <a href="#" onClick={()=> this.props.openSignUpForm()} className="btn-base-landing btn-red-landing">
+                            <span><b>Sign In</b></span>
+                        </a><br/>
+                        <ActionLink id="link-create-acc" href="#" onClick={()=> this.startCharacterCreation()} className="">
+                          <span style={{ textTransform: 'underline' }}>CREATE YOUR CHARACTER INSTEAD</span>
+                        </ActionLink>
+                   </p>
                       <iframe width="420" height="345" id="intro-video" src="https://www.youtube.com/embed/i8PJgSclIf0">
                       </iframe>
                     </div>
@@ -137,36 +132,6 @@ class LandingPage extends React.Component {
                 </div>
             </div>
             
-            <div className="modal fade" id="alpha" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 className="modal-title" id="myModalLabel">Alpha</h4>
-                        </div>
-                        <div className="modal-body">
-                            <p>The easiest way to learn and collaborate with your friends.
-                                The only Blockchain powered smart social network that propels you to the forefront of humankind.</p>
-                            <form>
-                                <div className="form-group">
-                                    <label htmlFor="alpha-name" className="control-label">Name:</label>
-                                    <input type="text" className="form-control" id="alpha-name" placeholder="Enter you name here"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="alpha-email" className="control-label">Email:</label>
-                                    <input type="text" className="form-control" id="alpha-email" placeholder="Enter you email here"/>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button id="cancel" type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                            <button id="ok" type="button" className="btn btn-primary">Join us</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
             <div className="row">
                 <div className="col-md-7">
 
