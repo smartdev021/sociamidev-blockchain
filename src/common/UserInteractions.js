fc966255ebdd796c7c628e02254dd53972693fb2
tm-315 <topguru315@gmail.com>
2018-03-25 20:18:52 +0300

    Added new SubTypes into 'UserInteractions'

--- src/common/UserInteractions.js
+++ src/common/UserInteractions.js
@@ -10,8 +10,10 @@ const UserInteractions = {
         
         PROG_TREE_VIEW: "progression_tree_view",
 
-        DEEPDIVE_START: "deepdive_start",
         DEEPDIVE_PREPARE: "deepdive_prepare",
+        DEEPDIVE_START: "deepdive_start",
+        DEEPDIVE_FINISH: "deepdive_finish",
+        DEEPDIVE_COMPLETE: "deepdive_complete",
     },
 }
 
