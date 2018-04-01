e751df354d75de7107b0928f4904d44dc27d5078
tm-315 <topguru315@gmail.com>
2018-04-01 18:27:12 +0300

    Illuminate & Deepdive popup fixed

--- src/theme/ThemeHeader.js
+++ src/theme/ThemeHeader.js
@@ -19,7 +19,7 @@ import ConfigMain from '~/configs/main'
 
 import { ToastContainer, toast } from 'react-toastify';
 
-import { withCookies, Cookies } from 'react-cookie';
+import PubSub from 'pubsub-js';
 
 class ThemeHeader extends React.Component {
 
@@ -41,32 +41,55 @@ class ThemeHeader extends React.Component {
         this.setState({ notificationsOpen: false });
     }
 
+    componentWillMount() {
+        this.PubsubEventsSubscribe();
+    }
+
+    componentWillUnmount() {
+        this.PubsubEventsUnSubscribe();
+    }
+
+    PubsubEventsSubscribe() {
+        if (!this.token_server_event_accounting_update) {
+            this.token_server_event_accounting_update = PubSub.subscribe("accounting_updated", this.serverEventAccountingUpdated.bind(this));
+        }
+    }
+
+    PubsubEventsUnSubscribe() {
+        if (this.token_server_event_accounting_update) {
+            PubSub.unsubscribe(this.token_server_event_accounting_update);
+            this.token_server_event_accounting_update = undefined;
+        }
+    }
+
+    serverEventAccountingUpdated(msg, data) {
+        if (data && data.numTokens) {
+            let source = "";
+
+            if (data.source) {
+                if (data.source.illuminate) {
+                    source = `for ${data.source.illuminate.name}`;
+                }
+                else if (data.source.deepdive) {
+                    source = `for ${data.source.deepdive.name}`;
+                }
+            }
+
+            this.showNotification(`Congratulations: You've earned ${data.numTokens} ${data.numTokens > 1 ? "tokens" : "token"} ${source}!!!`);
+        }
+    };
+
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.isAuthorized != this.props.isAuthorized) {
             if (this.props.isAuthorized) {
                 this.props.fetchUserActivities(this.props.currentUserID);
+                this.PubsubEventsUnSubscribe();
+                this.PubsubEventsSubscribe();
+            }
+            else {
+                this.PubsubEventsUnSubscribe();
             }
         }
-
-        if (prevProps.accounting.data.numTokens < this.props.accounting.data.numTokens) {
-
-            const { cookies } = this.props;
-            const numTokensFromCookies = cookies.get("tokens_total");
-
-            if (!numTokensFromCookies || this.props.accounting.data.numTokens > numTokensFromCookies) {
-                const amountEarned = this.props.accounting.data.numTokens - prevProps.accounting.data.numTokens;
-                this.showNotification(`Congratulations: You've earned ${amountEarned} ${amountEarned > 1 ? "tokens" : "token"}!!!`);
-
-                this.storeNumTokensInCookies(this.props.accounting.data.numTokens);
-            }
-        }
-    }
-
-    storeNumTokensInCookies(numTokens) {
-        const { cookies } = this.props;
-        let dateExpire = new Date();
-        dateExpire.setTime(dateExpire.getTime() + ConfigMain.getCookiesExpirationPeriod());
-        cookies.set("tokens_total", numTokens, { path: '/', expires: dateExpire });
     }
 
     showNotification(message) {
@@ -181,4 +204,4 @@ class ThemeHeader extends React.Component {
     }
 }
 
-export default withCookies(ThemeHeader);
+export default ThemeHeader;
