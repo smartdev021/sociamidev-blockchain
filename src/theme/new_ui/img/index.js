8ed4918c1c9eca05ee28530fa0b779ed3fcab9f6
tm-315 <topguru315@gmail.com>
2018-05-22 01:23:48 +0530

    Updated Landing Page

---
+++ src/theme/new_ui/img/index.js
@@ -0,0 +1,11 @@
+export default {
+  featured1: require("./featured-1.png"),
+  featured2: require("./featured-2.png"),
+  featured3: require("./featured-3.png"),
+  featured4: require("./featured-4.png"),
+  mainbg: require("./bg.png"),
+  footerbg: require("./footer-bg.png"),
+  identity: require("./identity.png"),
+  learning: require("./learning.png"),
+  social: require("./social.png")
+};
