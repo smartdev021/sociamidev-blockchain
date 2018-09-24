14ec795f3aff71bf9b823e9406f5e8913ec835a8
tm-315 <topguru315@gmail.com>
2018-09-24 14:32:21 +0530

    added new left section for achievements

--- src/theme/components/achievements/LeftNav.js
+++ src/theme/components/achievements/LeftNav.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LeftNav = (props) => {
   return (
-    <div className="col fixed-wp">
+    <div className="col fixed-wp achievements-left-wp">
       <div className="col-box-wp p-0">
         <div className="left-content">
           <div className="top-head">
@@ -11,30 +11,40 @@ const LeftNav = (props) => {
             </div>
             <span className="col-heading">{props.userProfile.firstName} {props.userProfile.lastName}</span>
           </div>
-          <ul>
+          <ul className="achievements-left-wap-links">
             <li>
               <a href="#">
-                <span className="icon-wp">
-                  <img src="https://s3.us-east-2.amazonaws.com/sociamibucket/assets/images/homepage/newsfeed-menu-icon.png" />
-                </span>
-                <p>News Feed</p>
+                <p>All</p>
               </a>
             </li>
             <li>
               <a href="#">
-                <span className="icon-wp">
-                  <img src="https://s3.us-east-2.amazonaws.com/sociamibucket/assets/images/homepage/messages-menu-icon.png" />
-                </span>
-                <p>Messages</p>
+                <p>General</p>
               </a>
             </li>
-            <div className="line-devider"></div>
+            <li className="active">
+              <a href="#">
+                <p>House</p>
+              </a>
+            </li>
             <li>
               <a href="#">
-                <span className="icon-wp">
-                  <img src="https://s3.us-east-2.amazonaws.com/sociamibucket/assets/images/homepage/team-menu-icon.png" />
-                </span>
-                <p>Team</p>
+                <p>Hero</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Zara</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Mango</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Apple</p>
               </a>
             </li>
           </ul>
