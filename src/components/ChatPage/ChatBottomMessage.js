6cdea564e7f2423acd28de682be92bfbfe87849f
tm-315 <topguru315@gmail.com>
2018-10-04 23:39:27 +0530

    Solve commit message code update

--- src/components/ChatPage/ChatBottomMessage.js
+++ src/components/ChatPage/ChatBottomMessage.js
@@ -1,34 +1,32 @@
-import React,{Component} from 'react';
+import React from 'react';
 
-class ChatBottomMessage extends Component{
-  render(){
-    return(
-      <div className="right-footer">
-        <div className="input-holder">
-          <input placeholder="Type a message"/>
-        </div>
-        <div className="chat-icons">
-          <ul>
-            <li>
-              <a className="fa fa-link"></a>
-            </li>
-            <li>
-              <a className="fa fa-picture-o"></a>
-            </li>
-            <li>
-              <a className="fa fa-paperclip"></a>
-            </li>
-            <li>
-              <a className="fa fa-smile-o"></a>
-            </li>
-            <li className="send-btn">
-              <a className="fa fa-play"></a>
-            </li>
-          </ul>
-        </div>
+const ChatBottomMessage = () => {
+  return(
+    <div className="right-footer">
+      <div className="input-holder">
+        <input placeholder="Type a message"/>
       </div>
-    )
-  }
+      <div className="chat-icons">
+        <ul>
+          <li>
+            <a className="fa fa-link"></a>
+          </li>
+          <li>
+            <a className="fa fa-picture-o"></a>
+          </li>
+          <li>
+            <a className="fa fa-paperclip"></a>
+          </li>
+          <li>
+            <a className="fa fa-smile-o"></a>
+          </li>
+          <li className="send-btn">
+            <a className="fa fa-play"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default ChatBottomMessage;
