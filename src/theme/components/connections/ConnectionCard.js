cf0f0e84e9a42a97f38b4c25bb2f1713ea8cbc0d
tm-315 <topguru315@gmail.com>
2018-11-03 00:35:41 +0530

    Send chat message from connections

--- src/theme/components/connections/ConnectionCard.js
+++ src/theme/components/connections/ConnectionCard.js
@@ -3,69 +3,77 @@ import PubSub from 'pubsub-js';
 
 let  fallbackProfilePic = 'https://s3.us-east-2.amazonaws.com/sociamibucket/assets/images/userProfile/default-profile.png';
 
-const openChatWindow = (user) => {
-  console.log('user', user)
-  const chatWindow = document.getElementById('chat-popout');
-  chatWindow.click();
-  const chatBoxElement = document.getElementById(user.id);
-  if (chatBoxElement) {
-    chatBoxElement.click();
-  } else {
-    PubSub.publish('OpenChat', user);
+class ConnectionCard extends React.Component{
+  
+  openChatWindow(connection){
+    let data = {}
+    data.user = {
+      _id: connection.id,
+      firstName: connection.firstName,
+      lastName: connection.lastName
+    }
+    const chatWindow = document.getElementById('chat-popout');
+    chatWindow.click();
+    const chatBoxElement = document.getElementById(data.user._id);
+    if (chatBoxElement) {
+      chatBoxElement.click();
+    } else {
+      PubSub.publish('OpenChat', data);
+    }
   }
-}
-
-const ConnectionCard = props => {
-  return (
-    <div className="connection-card">
-      <div className="connection-profile-pic">
-        <img
-          onClick={props.onClickCheckUserProfile}
-          src={props.connection.profilePic || fallbackProfilePic}
-          onError={e => { e.target.src = fallbackProfilePic; }} />
+  
+  render(){
+    return (
+      <div className="connection-card">
+        <div className="connection-profile-pic">
+          <img
+            onClick={this.props.onClickCheckUserProfile}
+            src={this.props.connection.profilePic || fallbackProfilePic}
+            onError={e => { e.target.src = fallbackProfilePic; }} />
+        </div>
+        <div className="connection-info">
+          <h1 onClick={this.props.onClickCheckUserProfile} title={`${this.props.connection.firstName} ${this.props.connection.lastName}`}>
+            {this.props.connection.firstName} {this.props.connection.lastName}
+          </h1>
+          <p>Innovation is widely known as a value which is worth pursuing</p>
+        </div>
+        <div className="connection-actions">
+          <a href="#" className="btn-prim" onClick={this.props.onPrimaryAction}>{this.props.actionName || 'Add'}</a>
+          {this.props.secondaryAction
+            && <a href="#" className="btn-prim" onClick={this.props.onSecondaryAction}>{this.props.secondaryAction}</a> }
+          <a href="#" className="btn-circ" onClick={()=>this.openChatWindow(this.props.connection)}>
+            <img src="https://s3.us-east-2.amazonaws.com/sociamibucket/assets/images/userProfile/send-arrow.png" alt="" />
+          </a>
+        </div>
+        <ul className="connection-details">
+          <li>
+            <span>Friends</span>
+            <div className="sql-badge">
+              <a href="#">{this.props.connection.connections.friendCount}</a>
+            </div>
+          </li>
+          <li>
+            <span>Progression Trees</span>
+            <div className="sql-badge">
+              <a href="#">{this.props.connection.connections.progressionCount}</a>
+            </div>
+          </li>
+          <li>
+            <span>Challenges</span>
+            <div className="sql-badge">
+              <a href="#">{this.props.connection.connections.projectCount}</a>
+            </div>
+          </li>
+          <li>
+            <span>Tasks</span>
+            <div className="sql-badge">
+              <a href="#">{this.props.connection.connections.taskCount}</a>
+            </div>
+          </li>
+        </ul>
       </div>
-      <div className="connection-info">
-        <h1 onClick={props.onClickCheckUserProfile} title={`${props.connection.firstName} ${props.connection.lastName}`}>
-          {props.connection.firstName} {props.connection.lastName}
-        </h1>
-        <p>Innovation is widely known as a value which is worth pursuing</p>
-      </div>
-      <div className="connection-actions">
-        <a href="#" className="btn-prim" onClick={props.onPrimaryAction}>{props.actionName || 'Add'}</a>
-        {props.secondaryAction
-          && <a href="#" className="btn-prim" onClick={props.onSecondaryAction}>{props.secondaryAction}</a> }
-        <a href="#" className="btn-circ" onClick={openChatWindow(props.connection)}>
-          <img src="https://s3.us-east-2.amazonaws.com/sociamibucket/assets/images/userProfile/send-arrow.png" alt="" />
-        </a>
-      </div>
-      <ul className="connection-details">
-        <li>
-          <span>Friends</span>
-          <div className="sql-badge">
-            <a href="#">{props.connection.connections.friendCount}</a>
-          </div>
-        </li>
-        <li>
-          <span>Progression Trees</span>
-          <div className="sql-badge">
-            <a href="#">{props.connection.connections.progressionCount}</a>
-          </div>
-        </li>
-        <li>
-          <span>Challenges</span>
-          <div className="sql-badge">
-            <a href="#">{props.connection.connections.projectCount}</a>
-          </div>
-        </li>
-        <li>
-          <span>Tasks</span>
-          <div className="sql-badge">
-            <a href="#">{props.connection.connections.taskCount}</a>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default ConnectionCard;
