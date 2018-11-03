ea2be1039bb2508512326a48505acf2b4a6cc279
tm-315 <topguru315@gmail.com>
2018-11-03 17:48:24 +0700

    trigger a Decode task from Signup using skill in story

--- src/theme/components/story/Story.js
+++ src/theme/components/story/Story.js
@@ -13,13 +13,19 @@ import LeftNav from '~/src/theme/components/homepage/LeftNav';
 import RightSection from '~/src/theme/components/homepage/RightSection';
 import Skills from './Skills';
 
+import TaskTypes from '~/src/common/TaskTypes';
+
 import { 
   fetchStories
 } from '~/src/redux/actions/story';
+import { saveTask } from '~/src/redux/actions/tasks';
 
 
 const profilePic =
   'https://s3.us-east-2.amazonaws.com/sociamibucket/assets/images/userProfile/default-profile.png';
+  const RandomInt = function RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
 class Story extends Component {
   constructor (props) {
@@ -35,8 +41,45 @@ class Story extends Component {
     mixpanel.track("Enter Story page");
   }
 
+  onSigupDecode(data){
+    const decode = {
+      type: TaskTypes.DECODE,
+      userName: `${this.props.userProfile.firstName} ${this.props.userProfile.lastName}`,
+      userID: this.props.userProfile._id,
+      isHidden: 0,
+      creator: {
+        _id: this.props.userProfile._id,
+        firstName: this.props.userProfile.firstName,
+        lastName: this.props.userProfile.lastName,
+      },
+      metaData: {
+        subject: {
+          skill: {
+            _id: data._id,
+            name: data.skill,
+          },
+        },
+        participants: [
+          {
+            user: {
+              _id: this.props.userProfile._id,
+              firstName: this.props.userProfile.firstName,
+              lastName: this.props.userProfile.lastName,
+            },
+            status: 'accepted',
+            isCreator: true,
+          },
+        ],
+        ratings: [],
+        time: Date.now(),
+        awardXP: RandomInt(30, 40),
+      },
+    };
+    this.props.saveTask(decode);
+  }
+
   renderSkillBox(skills) {
-    return _.map(skills, skill => <Skills key={skill._id} data={skill}/>)
+    return _.map(skills, skill => <Skills key={skill._id} data={skill} onSignup={(data) => this.onSigupDecode(data)}/>)
   }
 
   render () {
@@ -78,6 +121,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   fetchStories: bindActionCreators(fetchStories, dispatch),
+  saveTask: bindActionCreators(saveTask, dispatch)
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Story));
